ac0e14c36cd9620175005854baabe32992768e94
nitipon apaisri <nitipon.apaisri@gmail.com>
2021-05-31 20:56:54 +0200

    Working on task list

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -7,27 +7,23 @@ const DATA = [
    { title: "Do something", date: "2021-05-17" },
    { title: "Do something", date: "2021-05-17" },
 ];
-const ItemTitle = ({ title, date }) => (
+const ItemTitle = ({ title }) => (
    <View>
-      <Text>{title}</Text>
-      <Text>{date}</Text>
+      <Text style={style.taskTitle}>{title}</Text>
    </View>
 );
 
 const TaskList = () => {
    const renderItem = ({ item, index }) => (
-      <View>
-         <Text>
+      <View style={style.task}>
+         <Text style={style.taskTitle}>
             <ItemTitle title={item.title} />
-         </Text>
-         <Text>
-            <ItemTitle title={item.date} />
          </Text>
       </View>
    );
 
    return (
-      <View>
+      <View style={style.container}>
          <FlatList
             data={DATA}
             renderItem={renderItem}
@@ -37,4 +33,21 @@ const TaskList = () => {
    );
 };
 
+const style = StyleSheet.create({
+   container: {
+      flex: 1,
+      padding: 32,
+   },
+   task: {
+      padding: 16,
+      height: 72,
+      backgroundColor: "#ffffff",
+      borderRadius: 8,
+      marginBottom: 16,
+   },
+   taskTitle: {},
+   taskDate: {
+      fontSize: 8,
+   },
+});
 export default TaskList;
