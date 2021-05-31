6752aae47e32c3fd67eb6c6a87b4509d809159df
richard-iths <richard.forsberg@iths.se>
2021-05-31 14:52:15 +0200

    cleaned up some unused code in dashboard screen

--- src/components/screens/dashboard/dashboard.screen.jsx
+++ src/components/screens/dashboard/dashboard.screen.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Text, View, Image, StyleSheet, FlatList } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { FlatGrid } from "react-native-super-grid";
 import DashboardItem, {
   StylingOption,
